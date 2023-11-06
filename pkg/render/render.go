2a93f5ce892f23da2f76d5243c1c697ad7c81f1b
dverves <denver1033@gmail.com>
2023-11-06 22:50:36 +0200

    added layouts

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,18 +1,45 @@
 package render
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
 
-func RenderTmpl(w http.ResponseWriter, tmplName string) {
-	tmpl, err := template.ParseFiles("./template/"+tmplName, "./template/body.html")
-	if err != nil {
-		http.Error(w, "error parsing file"+err.Error(), http.StatusInternalServerError)
-	}
-	errTmpl := tmpl.Execute(w, nil)
-	if errTmpl != nil {
-		http.Error(w, "error executing"+errTmpl.Error(), http.StatusInternalServerError)
-	}
+var tc = make(map[string]*template.Template)
 
+func RenderTmpl(w http.ResponseWriter, t string) {
+	var tmpl *template.Template
+	var err error
+
+	_, inMap := tc[t]
+	if !inMap {
+		fmt.Println("We create and cache new template")
+		err = createTemplatCache(t)
+		if err != nil {
+			http.Error(w, "error Executing"+err.Error(), http.StatusInternalServerError)
+		}
+
+	} else {
+		fmt.Println("We use cached template")
+	}
+	tmpl = tc[t]
+	errExecute := tmpl.Execute(w, nil)
+	if errExecute != nil {
+		http.Error(w, "error Executing"+errExecute.Error(), http.StatusInternalServerError)
+
+	}
+}
+
+func createTemplatCache(t string) error {
+	templates := []string{
+		fmt.Sprintf("./template/%s", t),
+		"./template/body.html",
+	}
+	tmpl, err := template.ParseFiles(templates...)
+	if err != nil {
+		return err
+	}
+	tc[t] = tmpl
+	return nil
 }
